c17f4802cd9ef2399237cdfded531ee7193f8408
Aldair Andrade <harperafterhours@gmail.com>
2026-01-20 17:33:49 -0500

    inicio

---
+++ 02.-Variables/operadores_asignacion.py
@@ -0,0 +1,18 @@
+precio_total=0
+precio_total=precio_total + 2.99
+
+precio_total=precio_total+3.19
+
+print(precio_total)
+
+edad=18
+edad+=10
+print(edad)
+
+mensaje="Hola"
+mensaje = mensaje + " Bienvenido al curso de python" 
+print(mensaje)
+
+curso="El curso"
+curso+=" Es de nivel avanzado"
+print(curso)
