99c599412d85a55a9b3ac62ef8be2a9bffec80a1
Aldair Andrade <demianmaster2003@gmail.com>
2026-01-20 23:18:14 -0500

    feat.: Funciones

---
+++ 03.-Funciones/note_pythpn.py
@@ -0,0 +1,8 @@
+a=None
+print(type(a))
+
+def suma(a,b):
+    c=a-b
+
+resultado=suma(80,25)
+print(resultado)
---
+++ 03.-Funciones/retornar_valor_funtion.py
@@ -0,0 +1,7 @@
+def suma(a,b):
+    c=a+b
+    print(f"el resultado es: {c}")
+    return c
+
+resultado=suma(80,20)
+print(resultado)
