d8e8f591c83eb38ac779ef1a4e193008535839ad
Aldair Andrade <demianmaster2003@gmail.com>
2026-01-21 23:35:38 -0500

    feat

---
+++ 06.-Listas_py/funciones_lectura.py
@@ -0,0 +1,14 @@
+mi_lista=[10,60,5,90,20,10,11]
+
+#Busqueda de un elemento de lista por indice
+print(mi_lista[5])
+print(mi_lista[1])
+print(mi_lista[0])
+
+#Busqueda de un elemento de lsita pro el valor
+
+print(mi_lista.index(11))
+print(mi_lista.index(90))
+
+#Extraer valores por rango de una lista
+print(mi_lista[1:4])
