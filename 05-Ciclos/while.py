dc499196c5741cc69d63c73d1ac40ce8ab6712de
Aldair Andrade <harperafterhours@gmail.com>
2026-01-30 23:54:11 -0500

    v2

---
+++ 05-Ciclos/while.py
@@ -0,0 +1,20 @@
+while True:
+	print("dentro del scripts")
+	print("Selecciona una opcion: /n")
+	print("1-Saludar")
+	print("2-despedida")
+	print("3-Salir")
+
+
+	opcion=input("ingrese su opcion:")
+
+	print(opcion)
+
+	if opcion =="1":
+		print("Hola desde mi script")
+
+	elif opcion =="2":
+		print("Chao desde script")
+
+	else:
+		break
