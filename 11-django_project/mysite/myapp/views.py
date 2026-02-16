975e3e6967e6225f5c5c8618c639aa85ca3f88a4
Aldair Andrade <harperafterhours@gmail.com>
2026-02-16 12:33:53 -0500

    feat: django Templates

--- 11-django_project/mysite/myapp/views.py
+++ 11-django_project/mysite/myapp/views.py
@@ -1,21 +1,30 @@
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
 from django.shortcuts import render
-# Create your views here.
 
+# Create your views here.
 def index(request):
-    return render(request, 'index.html')   
+    title_inicio = 'Django Course!!'
+    return render(request, 'index.html', {
+        'title': title_inicio
+    })   
 
 def hello(request, id):
     print(id+100*2)
     return HttpResponse("<h1> Hello  %s</h1>" %id)
 
 def about(request):
-    return render(request, 'about.html')
+    username_dev= 'DairXP'
+    return render(request, 'about.html', {
+        'username': username_dev
+    })
 
 def projects(request):
-    projects= list(Project.objects.values())
-    return render(request, 'projects.html')
+    #projects= list(Project.objects.values())
+    projects= Project.objects.all()
+    return render(request, 'projects.html', {
+        'projects_temp':projects
+    })
 
 def tasks(request):
     #task=Task.objects.get(id=id)
