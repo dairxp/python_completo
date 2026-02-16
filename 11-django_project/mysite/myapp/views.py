f64edd8968f4464e2ccadfec17ed553a3c69d58f
Aldair Andrade <demianmaster2003@gmail.com>
2026-02-16 12:08:29 -0500

    feat: Añadir render html desde template.. django

--- 11-django_project/mysite/myapp/views.py
+++ 11-django_project/mysite/myapp/views.py
@@ -1,13 +1,22 @@
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index page")
+    return render(request, 'index.html')   
 
 def hello(request, id):
     print(id+100*2)
     return HttpResponse("<h1> Hello  %s</h1>" %id)
 
 def about(request):
-    return HttpResponse("about")
+    return render(request, 'about.html')
+
+def projects(request):
+    projects= list(Project.objects.values())
+    return render(request, 'projects.html')
+
+def tasks(request):
+    #task=Task.objects.get(id=id)
+    return render(request, 'tasks.html')
