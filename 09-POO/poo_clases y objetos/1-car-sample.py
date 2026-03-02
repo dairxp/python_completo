4395f84b52068a2944792c33f481d70d0651a502
Aldair Andrade <harperafterhours@gmail.com>
2026-03-02 10:20:20 -0500

    feat: __str_()

--- 09-POO/poo_clases y objetos/1-car-sample.py
+++ 09-POO/poo_clases y objetos/1-car-sample.py
@@ -1,49 +1,7 @@
-class CarStation:
-    def __init__(self, manufacturer=None, model=None, color='', cylinder=0.00):
-        #atributo oculto __
-        self.__manufacturer = manufacturer
-        self.__model = model
-        self.__color = color
-        self.__cylinder = cylinder
-        self._other = 'motor'
-    
-    def set_model(self, value):
-        self.__model = value
-    def get_model(self):
-        return self.__model
-    
-    @property
-    def model(self):
-        return self.__model
-    @model.setter
-    def model(self, value):
-        self.__model = value
-
-    def set_color(self, value):
-        self.__color = value
-    def get_color(self):
-        return self.__color
-
-    @property
-    def cylinder(self):
-        return self.__cylinder
-
-    @cylinder.setter
-    def cylinder(self, value):
-        self.__cylinder = value
-
-    def details(self):
-        detail = f'manufacturer: {self.__manufacturer} \n'
-        detail += f'model: {self.__model} \n'
-        detail += f'color: {self.__color} \n'
-        detail += f'cylinder: {self.__cylinder} \n\n'
-        return detail
-
-
-
+from car import Car
 #metodo pubico 
 #print(car.manufacturer)
-car = CarStation('subaru')
+car = Car('subaru')
 car.set_model('Terraneitor')
 car.set_color('Red')
 car.cylinder = 1.5
@@ -56,7 +14,7 @@ print(car.details())
 print(car.cylinder)
 
 
-mazda = CarStation('Mazda', 'BT-50', 'Black')
+mazda = Car('Mazda', 'BT-50', 'Black')
 mazda.cylinder= 1.9
 print(mazda.details())
 print(mazda.cylinder)
@@ -64,6 +22,9 @@ print(mazda.cylinder)
 mazda._other = 'Otro gato'
 print(mazda._other)
 
+print("============== metodo __str__ =================")
+print(car)
+print(repr(car))
 
 # mazda = CarStation()
 # mazda.manufacturer = 'Mazda'
