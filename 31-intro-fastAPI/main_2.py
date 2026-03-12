a77553f7bc87a42d6fb79955c506f3418c7155f5
Aldair Andrade <harperafterhours@gmail.com>
2026-03-11 23:19:29 -0500

    feat: POST fast api

--- 31-intro-fastAPI/main_2.py
+++ 31-intro-fastAPI/main_2.py
@@ -1,51 +1,61 @@
-from fastapi import FastAPI, status, Response
+from typing import List, Optional
+
 import uvicorn
-from typing import List,Optional
+from fastapi import FastAPI, Response, status
+from models.developer import Developer
 
 app = FastAPI()
 
 developers = []
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hola mundo"}
+
 
 @app.get("/developers")
 def read_developers():
     return developers
 
+
 @app.get("/developers/{id}")
-def read_developers(id:int):
+def read_developer(id: int):
     for developer in developers:
-        if developer['id'] == id:
+        #if developer["id"] == id:
+        if developer.id == id:
             return developer
     return "Developer no encontrado"
 
+
 @app.get("/developers/{id}/skills")
-def read_developers(id:int):
+def read_skills(id: int):
     for developer in developers:
-        if developer['id'] == id:
-            return developer['skills']
+        if developer.id  == id:
+            return developer.skills
     return "Developer no encontrado"
+
 
 @app.get("/developers/{id}/experience")
-def read_developers(id:int):
+def read_experience(id: int):
     for developer in developers:
-        if developer['id'] == id:
-            return developer['experience']
+        if developer.id == id:
+            return developer.experience
     return "Developer no encontrado"
+
 
 @app.get("/developers/{id}/languages")
-def read_developers(id:int):
+def read_languajes(id: int):
     for developer in developers:
-        if developer['id'] == id:
-            return developer['languages']
+        if developer.id == id:
+            return developer.languages
     return "Developer no encontrado"
 
-## PoST
 
-@app.post("/developers")
-def create_developer(id:int, name:str, country:str, age:int, experience: List[dict], skills: List[dict], languages: List[dict]):
+## PoST sin clase
+"""
+@app.post("/developers_sin")
+def create__new_developer_sin_clase(id:int, name:str, country:str, age:int, experience: List[dict], skills: List[dict], languages: List[dict]):
     developers.append({
         "id": id,
         "name": name,
@@ -57,7 +67,22 @@ def create_developer(id:int, name:str, country:str, age:int, experience: List[di
     })
     return developers
 
+"""
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='127.0.0.1', port=5000)
+@app.post("/developers")
+def create__new_developer(developer: Developer):
+    developers.append(developer)
+    return developers
 
+
+@app.delete("/developers/{id}")
+def delete_developer(id: int):
+    for developer in developers:
+        if developer.id == id:
+            developers.remove(developer)
+            return developers
+    return "No encontrado"
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=5000)
